ed67c14cd7d7523a9d0e532184765747306cd856
Alfred-Onuada <aonuada5@gmail.com>
2025-11-14 13:01:32 +0100

    playing with outputs

--- 001_getting_started/main.tf
+++ 001_getting_started/main.tf
@@ -34,3 +34,8 @@ resource "aws_instance" "my_server" {
     Environment = local.environment
   }
 }
+
+# this creates an output of name instance_ip_addr with value of the public IP of the created instance
+output "instance_ip_addr" {
+  value = aws_instance.my_server.public_ip
+}
